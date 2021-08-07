8f20164e39d5762a518da234cfadd112581381de
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2021-08-07 17:19:39 +0500

    calculator init

--- App.js
+++ App.js
@@ -1,27 +1,66 @@
-import React from "react";
-import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
 
 export default function App() {
+  const [answer, setAnswer] = useState(0);
+  const [operand, setOperand] = useState(undefined);
+
+  const add = (num) => {
+    return answer + num;
+  };
+
+  const sub = (num) => {
+    return Math.max(answer - num, 0);
+  };
+
+  const mul = (num) => {
+    return answer * num;
+  };
+
+  const handelAns = (num) => {
+    if (answer && num && operand) setAnswer(operand(num));
+    else setAnswer(num);
+  };
+
+  const handelOperand = (operand) => {
+    if (answer) setOperand(operand);
+    else setOp(undefined);
+  };
+
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder="Enter Anything" style={styles.input} />
-        <Button title="ADD" />
+      <Text style={styles.answer}>{answer}</Text>
+      {[...Array(5)].map((_, i) => (
+        <View style={styles.btn} key={i}>
+          <Button title={(i + 1).toString()} onPress={() => handelAns(i + 1)} />
+        </View>
+      ))}
+      <View style={styles.btn}>
+        <Button
+          title="CLEAR"
+          color="rgb(255, 0, 0)"
+          onPress={() => handelAns(0)}
+        />
       </View>
-      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  input: {
-    padding: 5,
-    marginVertical: 10,
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-    width: "100%",
+  answer: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  btn: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    margin: 5,
   },
 });
