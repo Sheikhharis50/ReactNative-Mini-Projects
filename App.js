a22c2c80a588bc45c4ebcc7f3a8d743adaa75bcb
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2021-08-07 17:21:36 +0500

    root init

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
